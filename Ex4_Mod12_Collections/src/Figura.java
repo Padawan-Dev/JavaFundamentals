73f94a2c81196c2d7e5bf7fdd5c7e5d06449cfed
padawandev <born4codex@gmail.com>
2021-06-16 21:16:44 -0300

    Exercicio de calculo de area de uma figura complexa.

---
+++ Ex4_Mod12_Collections/src/Figura.java
@@ -0,0 +1,13 @@
+
+/*
+ * Representa uma figura
+ */
+public class Figura {
+
+	/*
+	 * Calcula a área da figura. Este método é sobrecrito pelas subclasses
+	 */
+	public double calcularArea() {
+		return 0.0;
+	}
+}
